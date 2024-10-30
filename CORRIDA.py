cc965a5392f5c1a1ddc1a2f6f6b5f79344c393b7
Kennedy Cordeiro <cursogrupo66@gmail.com>
2024-10-30 14:15:59 -0400

    primeiro comite

--- CORRIDA.py
+++ CORRIDA.py
@@ -47,6 +47,7 @@ pygame.display.set_caption('Corida')
 relogio = pygame.time.Clock()
 
 cont = cont1 = cont2 = cont3 = cont4 = cont5 = cont6 = 0
+
 speed = 3
  
 sprite_image2 = pygame.image.load('car 3.png')
@@ -90,6 +91,7 @@ novo_tamanho = (imagem_fundo.get_width() * 7, imagem_fundo.get_height() * 10)
 imagem_aumentada = pygame.transform.scale(imagem_fundo, novo_tamanho)
 
 while True:
+    
     cont += 3
     cont1 += 2
     cont2 += 1.7
@@ -97,7 +99,7 @@ while True:
     cont4 += 0.7
     cont5 += 1.5
     cont6 += 2.3
-    relogio.tick(120)
+    relogio.tick(160)
     imagem_fundo = pygame.image.load('rua.png')
     mesagem = f'Verde: {pontos}'
     mesagem2 = f'Vermelho: {pontos2}'
@@ -110,7 +112,7 @@ while True:
             pygame.quit()
             exit()
 
-   
+    
     sprite_rect.topleft = (x_amarelo,cont)
     sprite_rect1.topleft = (x_amarelo1,cont1)
     sprite_rect2.topleft = (x_amarelo2,cont2)
@@ -146,7 +148,7 @@ while True:
         tela.blit(texto_formatado3,(largura/2.3, altura/2.6))
         #barulho_colisao.play()
         break
-
+        
     if sprite_rectcar2.colliderect(sprite_rect) or sprite_rectcar2.colliderect(sprite_rect1) or sprite_rectcar2.colliderect(sprite_rect2) or sprite_rectcar2.colliderect(sprite_rect3) or sprite_rectcar2.colliderect(sprite_rect4) or sprite_rectcar2.colliderect(sprite_rect5) or sprite_rectcar2.colliderect(sprite_rect6):
         tela.blit(texto_formatado3,(largura/2.3, altura/2.6))
         #barulho_colisao.play()
